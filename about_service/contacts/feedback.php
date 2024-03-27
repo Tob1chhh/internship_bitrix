a914adba14e9910c48123c708f606089d791497d
Tob1chhh <muzaloff_ivan@mail.ru>
2024-03-27 12:33:06 +0300

    Task 5: Placement of components

--- about_service/contacts/feedback.php
+++ about_service/contacts/feedback.php
@@ -3,4 +3,14 @@ require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 $APPLICATION->SetPageProperty("title", "Обратная связь");
 $APPLICATION->SetPageProperty("description", "Обратная связь");
 $APPLICATION->SetTitle("Обратная связь");
-?>Страница "Обратной связи"<?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
+?><?$APPLICATION->IncludeComponent(
+	"bitrix:main.feedback",
+	"",
+	Array(
+		"EMAIL_TO" => "draveone73@gmail.com",
+		"EVENT_MESSAGE_ID" => array(),
+		"OK_TEXT" => "Спасибо, ваше сообщение принято.",
+		"REQUIRED_FIELDS" => array("EMAIL","MESSAGE"),
+		"USE_CAPTCHA" => "Y"
+	)
+);?><?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
--- urlrewrite.php
+++ urlrewrite.php
@@ -1,5 +1,29 @@
 <?php
 $arUrlRewrite=array (
+  11 => 
+  array (
+    'CONDITION' => '#^/account_seller/seller_ads/#',
+    'RULE' => '',
+    'ID' => 'bitrix:news',
+    'PATH' => '/account_seller/seller_ads/index.php',
+    'SORT' => 100,
+  ),
+  9 => 
+  array (
+    'CONDITION' => '#^/about_service/jobs/#',
+    'RULE' => '',
+    'ID' => 'bitrix:news',
+    'PATH' => '/about_service/jobs/index.php',
+    'SORT' => 100,
+  ),
+  10 => 
+  array (
+    'CONDITION' => '#^/about_service/news/#',
+    'RULE' => '',
+    'ID' => 'bitrix:news',
+    'PATH' => '/about_service/news/index.php',
+    'SORT' => 100,
+  ),
   0 => 
   array (
     'CONDITION' => '#^/services/#',
@@ -16,12 +40,20 @@ $arUrlRewrite=array (
     'PATH' => '/products/index.php',
     'SORT' => 100,
   ),
-  2 => 
+  7 => 
   array (
     'CONDITION' => '#^/news/#',
     'RULE' => '',
     'ID' => 'bitrix:news',
     'PATH' => '/news/index.php',
+    'SORT' => 100,
+  ),
+  12 => 
+  array (
+    'CONDITION' => '#^/ads/#',
+    'RULE' => '',
+    'ID' => 'bitrix:news',
+    'PATH' => '/ads/index.php',
     'SORT' => 100,
   ),
 );
